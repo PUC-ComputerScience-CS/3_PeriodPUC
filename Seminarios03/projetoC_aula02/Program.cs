475dfd46e4d4bd147e9538e064a496f42ef8d7ae
Maria Clara Bertozi <178790393+MC-Bertozi@users.noreply.github.com>
2025-05-06 11:14:46 -0300

    Add files via upload

---
+++ Seminarios03/projetoC_aula02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        //vezes que o dado será lançado
+        Console.Write("Quantas vezes o dado sera lançado?(digite um numero inteiro): ");
+        string vezesAux = Console.ReadLine(); 
+        int vezes = int.Parse(vezesAux);
+
+        for(int i=1; i <= vezes; i++){
+            //valor do resultado
+            Random num = new Random();
+            int resultado = num.Next(1, 20);
+            Console.WriteLine($"Rolagem {i}: {resultado}\n");
+        }
+    }
+}
